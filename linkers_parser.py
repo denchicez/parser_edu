6ecd49aa8324ec2bda8ca9a40ee63fb244d122b1
Egor Bocharov <34909746+egor-bystepdev@users.noreply.github.com>
2020-11-17 17:50:07 +0300

    Update linkers_parser.py

--- linkers_parser.py
+++ linkers_parser.py
@@ -2,10 +2,15 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 def get_links_from_page(url):
-    resp = urllib.request.urlopen(url)
-    soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'))
     links = set()
-    for link in soup.find_all('a', href=True):
-        links.add(link['href'])
+    try:
+        resp = urllib.request.urlopen(url)
+        soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'))
+    except:
+        return links
+    try:
+        for link in soup.find_all('a', href=True):
+            links.add(link['href'])
+    except:
+        return links
     return links
-
