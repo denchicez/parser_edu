6a6d18d4a57854465ed8be673b53c2e67f09d22a
Egor Bocharov <34909746+egor-bystepdev@users.noreply.github.com>
2020-11-21 11:10:49 +0300

    Update social_parser.py

--- social_parser.py
+++ social_parser.py
@@ -1,7 +1,6 @@
-
 def get_social_links(links):
     prefixes = [['https://www.youtube.com/c/', 'https://www.youtube.com/channel/', 'https://www.youtube.com/user/'], ['https://vk.com/'], ['https://www.instagram.com/'], ['https://www.facebook.com/groups/', 'https://ru-ru.facebook.com/groups/'] ]
-    socials = [''] * len(prefixes)
+    socials = [[]] * len(prefixes)
     for link in links:
         for i in range(len(prefixes)):
             val = ''
@@ -9,16 +8,37 @@ def get_social_links(links):
                 if (link.find(media) != -1):
                     val = link
             if (len(val) != 0):
-                socials[i] = val
+                socials[i].append(val)
     result = {}
     if (len(socials[0]) != 0):
-        result['youtube'] = socials[0]
+        resLink = socials[0][0]
+        val = -1
+        for link in socials[0]:
+            follow = 0 # заменить за GetYouTube
+            if (follow > val):
+                val = follow
+                resLink = link
+        result['youtube'] = resLink
     if (len(socials[1]) != 0):
-        result['vk'] = socials[1]    
+        resLink = socials[0][0]
+        val = -1
+        for link in socials[0]:
+            follow = 0 # заменить за GetVk
+            if (follow > val):
+                val = follow
+                resLink = link
+        result['vk'] = resLink   
     if (len(socials[2]) != 0):
-        result['inst'] = socials[2]   
+        resLink = socials[0][0]
+        val = -1
+        for link in socials[0]:
+            follow = 0 # заменить за GetInst
+            if (follow > val):
+                val = follow
+                resLink = link
+        result['inst'] = resLink  
     if (len(socials[3]) != 0):
-        result['facebook'] = socials[3]       
+        result['facebook'] = socials[3][0] # так как facebook не получается, берем единственную ссылку       
     return result
 my_links = {'https://www.instagram.com/_egor.bocharov/?hl=ru', 'https://vk.com/countryballs_re', 'https://ru-ru.facebook.com/groups/363858560447388/', 'https://www.youtube.com/c/Spoontamer'}
 print(get_social_links(my_links))
